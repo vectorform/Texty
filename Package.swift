396d13cbfb7a328a666a993a79e178925cfc4cb6
Rafael Aguilera <Raguilera@vectorform.com>
2020-08-28 12:12:28 -0400

    switching to use path not dependency.

--- Package.swift
+++ Package.swift
@@ -6,23 +6,10 @@ import PackageDescription
 let package = Package(
     name: "Texty",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Texty",
-            targets: ["Texty"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "Texty", targets: ["Texty"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Texty",
-            dependencies: ["Source"]),
-        .testTarget(
-            name: "TextyTests",
-            dependencies: ["Texty"]),
+        .target(name: "Texty", path: "Source"),
+        .testTarget(name: "TextyTests", dependencies: ["Texty"]),
     ]
 )
